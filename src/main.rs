9ffc8714390269241504648d56d80769f032ffd4
William Terry <Tezza48is@gmail.com>
2021-01-23 01:28:16 +0000

    Bad discreet collisions and somewhat plastic

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,56 @@
+use bevy::{diagnostic::{FrameTimeDiagnosticsPlugin, PrintDiagnosticsPlugin}, prelude::*, render::camera::Camera};
 use core::f32;
-use bevy::{asset::Asset, math::vec3, prelude::*};
 
 /*
  * TODO WT:
-    * Planets random colours.
-    * Planets have gravity.
+ * Planets random colours.
+ * Planets have gravity.
  */
 
 fn main() {
     App::build()
         .add_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
+        // .add_plugin(FrameTimeDiagnosticsPlugin)
+        // .add_plugin(PrintDiagnosticsPlugin::default())
         .add_startup_system(startup_system.system())
-        .add_system(gravity.system())
+        // .add_system(update_camera.system())
+        .add_system_to_stage(stage::UPDATE, gravity.system())
+        .add_system_to_stage(stage::UPDATE, apply_velocity.system())
         .run();
 }
-
 
 fn startup_system(
     commands: &mut Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    let m = 1000.0;
+    let sun_rad = calc_rad(m, 4.0);
+    let target_ball = spawn_ball(
+        commands,
+        &mut meshes,
+        &mut materials,
+        Color::AZURE,
+        sun_rad,
+        m,
+        Vec3::zero(),
+    );
+
     commands
-    .spawn(Camera3dBundle {
-        transform: Transform::from_translation(Vec3::new(0.0, 100.0, 0.0)).looking_at(-Vec3::unit_y(), Vec3::unit_z()),
-        ..Default::default()
-    }).spawn(LightBundle {
-        light: Light {
-            color: Color::WHITE,
+        .spawn(Camera3dBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 100.0, 0.0))
+                .looking_at(-Vec3::unit_y(), Vec3::unit_z()),
             ..Default::default()
-        },
-        ..Default::default()
-    });
+        })
+        // .insert_resource(CameraFollow(target_ball.unwrap(), 100.0))
+        .spawn(LightBundle {
+            light: Light {
+                color: Color::WHITE,
+                ..Default::default()
+            },
+            ..Default::default()
+        });
 
     let rad = 200.0;
     let num_objects = 200;
@@ -44,23 +62,54 @@ fn startup_system(
     for _ in 0..num_objects {
         let x = rng.gen::<f32>() * rad - rad / 2.0;
         let y = rng.gen::<f32>() * rad - rad / 2.0;
+        // let y = 0.0;
         let z = rng.gen::<f32>() * rad - rad / 2.0;
+
+        let vec = Vec3::new(x, y, z);
+        if vec.length() < sun_rad {
+            continue;
+        }
 
         let m = rng.gen::<f32>() * 10.0 + 1.0;
 
-        spawn_ball(commands, &mut meshes, &mut materials, Color::WHITE, calc_rad(m, 1.0), m, Vec3::new(x, y, z));
+        spawn_ball(
+            commands,
+            &mut meshes,
+            &mut materials,
+            Color::WHITE,
+            calc_rad(m, 1.0),
+            m,
+            vec,
+        );
     }
-
-    // spawn_ball(commands, &mut meshes, &mut materials, Color::AZURE, 2.0, 10000.0, Vec3::zero());
-    // spawn_ball(commands, &mut meshes, &mut materials, Color::RED, 1.0, 1.0, Vec3::new(10.0, 0.0, 10.0));
-    // spawn_ball(commands, &mut meshes, &mut materials, Color::PURPLE, 1.0, 1.0, Vec3::new(3.0, 0.0, -15.0));
-    // spawn_ball(commands, &mut meshes, &mut materials, Color::GREEN, 1.0, 1.0, Vec3::new(6.0, 0.0, -4.0));
 }
 
 struct Planet {
     radius: f32,
     mass: f32,
     velocity: Vec3,
+}
+
+struct CameraFollow(Entity, f32);
+
+fn update_camera(
+    cam_follow: Res<CameraFollow>,
+    mut queries: QuerySet<(
+        Query<&Transform>,
+        Query<&mut Transform, With<Camera>>
+    )>,
+) {
+    let trans = if let Ok(transform) = queries.q0().get(cam_follow.0) {
+        transform.translation
+    } else {
+        return;
+    };
+
+    for mut cam_trans in queries.q1_mut().iter_mut() {
+        cam_trans.translation.x = trans.x;
+        cam_trans.translation.y = trans.y + cam_follow.1;
+        cam_trans.translation.z = trans.z;
+    }
 }
 
 fn spawn_ball(
@@ -72,25 +121,27 @@ fn spawn_ball(
     mass: f32,
     translation: Vec3,
 ) -> Option<Entity> {
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Icosphere {
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Icosphere {
+                radius,
+                subdivisions: 5,
+            })),
+            material: materials.add(StandardMaterial {
+                albedo: color,
+                albedo_texture: None,
+                shaded: true,
+            }),
+            transform: Transform::from_translation(translation),
+            ..Default::default()
+        })
+        .with(Planet {
             radius,
-            subdivisions: 5,
-        })),
-        material: materials.add(StandardMaterial {
-            albedo: color,
-            albedo_texture: None,
-            shaded: true,
-        }),
-        transform: Transform::from_translation(translation),
-        ..Default::default()
-    }).with(Planet {
-        radius,
-        mass,
-        velocity: Vec3::zero(),
-    }).current_entity()
+            mass,
+            velocity: Vec3::zero(),
+        })
+        .current_entity()
 }
-
 
 // f = m1 a
 // f = G(m1 m2) / r^2
@@ -99,13 +150,13 @@ fn spawn_ball(
 // r^2 a = G m2
 // a = G m2 / r^2
 
-fn gravity(
-    mut query: Query<(Entity, &mut Planet, &mut Transform)>,
-    time: Res<Time>,
-) {
-    let mass_positions: Vec<_> = query.iter_mut().map(|(entity, planet, transform)| (entity, planet.mass, transform.translation)).collect();
+fn gravity(mut query: Query<(Entity, &mut Planet, &mut Transform)>, time: Res<Time>) {
+    let mass_positions: Vec<_> = query
+        .iter_mut()
+        .map(|(entity, planet, transform)| (entity, planet.mass, transform.translation))
+        .collect();
 
-    let gravity_constant = 10.0;
+    let gravity_constant = 1.0;
 
     let delta_seconds = time.delta_seconds();
 
@@ -113,7 +164,9 @@ fn gravity(
         let mut accel = Vec3::zero();
 
         for (e_other, mass, translation) in mass_positions.iter() {
-            if entity == *e_other { continue; }
+            if entity == *e_other {
+                continue;
+            }
 
             let vec_to: Vec3 = *translation - transform.translation;
 
@@ -122,8 +175,34 @@ fn gravity(
         }
 
         planet.velocity += accel * delta_seconds;
-        transform.translation += planet.velocity * delta_seconds;
     }
+}
+
+fn apply_velocity(mut query: Query<(Entity, &mut Planet, &mut Transform)>, time: Res<Time>) {
+    let pos_rads: Vec<_> = query
+        .iter_mut()
+        .map(|(entity, planet, trans)| (entity, trans.translation, planet.radius))
+        .collect();
+
+    for (entity, mut planet, mut transform) in query.iter_mut() {
+        pos_rads.iter().for_each(|(e, pos, rad)| {
+            if entity == *e {
+                return;
+            }
+
+            let vec_to = *pos - transform.translation;
+            if vec_to.length() - (planet.radius + rad) < 0.0 {
+                planet.velocity = reflect_vec(planet.velocity, vec_to.normalize()) * 0.995;
+            }
+        });
+
+        // TODO WT: CCD
+        transform.translation += planet.velocity * time.delta_seconds();
+    }
+}
+
+fn reflect_vec(vector: Vec3, normal: Vec3) -> Vec3 {
+    -2.0 * (vector.dot(normal)) * normal + vector
 }
 
 fn calc_rad(mass: f32, density: f32) -> f32 {
@@ -138,4 +217,3 @@ fn calc_rad(mass: f32, density: f32) -> f32 {
 // v / r^3 = 4pi/3
 // 3v = 4pir^3
 // root3(3v/4pi) = r
-
